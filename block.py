bb04f5ffe79637a610bb7371011cbd6df20adaec
amaurs <amaury.gtz@gmail.com>
2018-05-29 17:23:54 -0500

    Convergence of 4 sources of evidence.

--- block.py
+++ block.py
@@ -9,6 +9,8 @@ src_3 = rasterio.open(file_template % 4)
 src_4 = rasterio.open(file_template % 5)
 src_5 = rasterio.open(file_6)
 src_6 = rasterio.open(file_bits)
+water = rasterio.open('final_global_surface_water.tif')
+mangroves = rasterio.open('final_mangroves.tif')
 
 
 profile = src_1.profile
@@ -26,6 +28,8 @@ for ji, window in src_1.block_windows(1):
     block_4 = src_4.read(window=window)
     block_5 = src_5.read(window=window)
     block_6 = src_6.read(window=window)
+    block_water = water.read(window=window)
+    block_mangroves = mangroves.read(window=window)
     stack = numpy.vstack((block_1,
                           block_2,
                           block_3,
@@ -34,6 +38,14 @@ for ji, window in src_1.block_windows(1):
                           block_6))
     mask = numpy.logical_and(numpy.bitwise_and.reduce(stack) == stack[0],
                              numpy.bitwise_or.reduce(stack) == stack[0])
-    to_write = numpy.full(block_1.shape, 255).astype(rasterio.uint8)
+
+    mask_water = block_water == 32 # 32 is the class for water
+    mask_mangroves = block_mangroves == 9 # 32 is the class for mangrove
+
+    print(mask_water.shape)
+
+    to_write = numpy.full(block_1.shape, 0).astype(rasterio.uint8)
     to_write[:,mask] = block_1[:,mask]
+    to_write[mask_mangroves] = 9
+    to_write[mask_water] = 32
     output.write(to_write, window=window)
